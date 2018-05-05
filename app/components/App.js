702a27c37087fe533669426ab423ecded4a1695e
emtseng <etseng42@gmail.com>
2018-05-05 17:28:55 -0400

    styles properly

--- app/components/App.js
+++ app/components/App.js
@@ -30,59 +30,59 @@ export default class App extends Component {
   }
   componentWillMount() {
     axios.get('/api/weather')
-    .then(res => {
-      this.setState({
-        weather: res.data.weather,
-        temp: res.data.temp,
-        weather_icon: res.data.weather_icon
+      .then(res => {
+        this.setState({
+          weather: res.data.weather,
+          temp: res.data.temp,
+          weather_icon: res.data.weather_icon
+        })
+        return axios.get('/api/tweets')
       })
-      return axios.get('/api/tweets')
-    })
-    .then(res => {
-      this.setState({
-        timeOfDay: this.getTimeOfDay(),
-        tweets: res.data,
-        trains: getDelays(this.state.trains, res.data),
-        max_id: res.data[0].id
+      .then(res => {
+        this.setState({
+          timeOfDay: this.getTimeOfDay(),
+          tweets: res.data,
+          trains: getDelays(this.state.trains, res.data),
+          max_id: res.data[0].id
+        })
       })
-    })
   }
   componentDidMount() {
     setInterval(() => {
       axios.get('/api/tweets')
-      .then(res => {
-        var tweets = res.data
-          , max_id = tweets[0].id
-        if (max_id > this.state.max_id) {
-          console.log('found new tweet, updating')
-          this.setState({
-            tweets,
-            trains: getDelays(this.state.trains, tweets),
-            max_id
-          })
-        } else {
-          console.log('found no new tweets')
-        }
-      })
+        .then(res => {
+          var tweets = res.data
+            , max_id = tweets[0].id
+          if (max_id > this.state.max_id) {
+            console.log('found new tweet, updating')
+            this.setState({
+              tweets,
+              trains: getDelays(this.state.trains, tweets),
+              max_id
+            })
+          } else {
+            console.log('found no new tweets')
+          }
+        })
     }, 120000)
   }
   render() {
     return (
-      <div>
+      <div id="app">
         <div id="greeting">
           Good {this.state.timeOfDay}!
-        </div>
-        <div id="weather">
-          <img src={this.state.weather_icon} />
-          {
-            this.state.weather
-          }
-          {
-            this.state.temperature_string
-          }
-          {
-            this.state.temp
-          }
+          <div id="weather">
+            <img src={this.state.weather_icon} />
+            {
+              this.state.weather
+            }
+            {
+              this.state.temperature_string
+            }
+            {
+              this.state.temp
+            }
+          </div>
         </div>
         <div id="trains">
           {
@@ -94,15 +94,19 @@ export default class App extends Component {
           }
         </div>
         <hr />
-        <div>
-          {
-            this.state.tweets ? this.state.tweets.map(tweet => (
-              <div className="tweet" style={{ margin: '1em' }}>
-                {tweet.text}
-              </div>
-            )) : null
-          }
-        </div>
+        {
+          /*
+          <div>
+            {
+              this.state.tweets ? this.state.tweets.map(tweet => (
+                <div className="tweet" style={{ margin: '1em' }}>
+                  {tweet.text}
+                </div>
+              )) : null
+            }
+          </div>
+          */
+        }
       </div>
     )
   }

2e21a384d7e677fdc81acb6a815747ae20265371
emtseng <etseng42@gmail.com>
2018-05-06 18:28:50 -0400

    handles errors

--- sensor.js
+++ sensor.js
@@ -2,7 +2,7 @@ var SerialPort = require('serialport'); // serial library
 var readLine = SerialPort.parsers.Readline; // read serial data as lines
 var hdmiOn = true;
 var exec = require('child_process').exec;
-console.log ('got here');
+console.log('got here');
 // start the serial port connection and read on newlines
 const serial = new SerialPort('/dev/ttyUSB0', {
   baudRate: 9600
@@ -15,20 +15,27 @@ const parser = new readLine({
 // Read data that is available on the serial port and send it to the websocket
 serial.pipe(parser);
 serial.on('open', function () {
-    console.log('~Port is open.');
-      parser.on('data', function (data) { //if person is present and HDMI is OFF, turn ON
-        if (data === 'PRESENT'  && !hdmiOn) {  
-          exec("tvservice -p", function (error, stdout, stderr) {
-      		console.log("Executed tvservice -p");
-              hdmiOn = true;
-          }
-        )}else if (data === 'AWAY' && hdmiOn){ //if Person is AWAY and HDMI is ON, turnoff
-          exec("tvservice -o", function (error, stdout, stderr) {
-      		console.log("Executed tvservice -o");
-              hdmiOn = false;
+  console.log('~Port is open.');
+  parser.on('data', function (data) { //if person is present and HDMI is OFF, turn ON
+    if (data === 'PRESENT' && !hdmiOn) {
+      exec("tvservice -p", function (error, stdout, stderr) {
+        if (error) {
+          console.error(error)
+        }
+        console.log("Executed tvservice -p");
+        hdmiOn = true;
+      }
+      )
+    } else if (data === 'AWAY' && hdmiOn) { //if Person is AWAY and HDMI is ON, turnoff
+      exec("tvservice -o", function (error, stdout, stderr) {
+        if (error) {
+          console.error(error)
+        }
+        console.log("Executed tvservice -o");
+        hdmiOn = false;
 
-              }) 
-          }
-     })    
-  });
+      })
+    }
+  })
+});
 

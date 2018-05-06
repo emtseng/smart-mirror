47b7e5620c22aaa4e4afae280233d39a327d6522
emtseng <etseng42@gmail.com>
2018-05-06 18:54:09 -0400

    debugs

--- sensor.js
+++ sensor.js
@@ -1,41 +1,36 @@
 var SerialPort = require('serialport'); // serial library
-var readLine = SerialPort.parsers.Readline; // read serial data as lines
+// var readLine = SerialPort.parsers.Readline; // read serial data as lines
 var hdmiOn = true;
 var exec = require('child_process').exec;
-console.log('got here');
 // start the serial port connection and read on newlines
 const serial = new SerialPort('/dev/ttyUSB0', {
-  baudRate: 9600
-});
-const parser = new readLine({
-  delimiter: '\n'
+  baudRate: 9600,
+  parser: new SerialPort.parsers.Readline('\n')
 });
 
 // Read data that is available on the serial port and send it to the websocket
-serial.pipe(parser);
+// serial.pipe(parser);
 serial.on('open', function () {
   console.log('~Port is open.');
-  parser.on('data', function (data) { //if person is present and HDMI is OFF, turn ON
-    console.log('data', data)
-    if (data === 'PRESENT' && !hdmiOn) {
-      exec("tvservice -p", function (error, stdout, stderr) {
-        if (error) {
-          console.error(error)
-        }
-        console.log("Executed tvservice -p");
-        hdmiOn = true;
+})
+
+serial.on('data', function (data) {
+  console.log('Registered data', data)
+  if (data === 'PRESENT' && !hdmiOn) { //if person is present and HDMI is OFF, turn ON
+    exec("tvservice -p", function (error, stdout, stderr) {
+      if (error) {
+        console.error(error)
       }
-      )
-    } else if (data === 'AWAY' && hdmiOn) { //if Person is AWAY and HDMI is ON, turnoff
-      exec("tvservice -o", function (error, stdout, stderr) {
-        if (error) {
-          console.error(error)
-        }
-        console.log("Executed tvservice -o");
-        hdmiOn = false;
-
-      })
-    }
-  })
-});
-
+      console.log("Executed tvservice -p");
+      hdmiOn = true;
+    })
+  } else if (data === 'AWAY' && hdmiOn) { //if Person is AWAY and HDMI is ON, turnoff
+    exec("tvservice -o", function (error, stdout, stderr) {
+      if (error) {
+        console.error(error)
+      }
+      console.log("Executed tvservice -o");
+      hdmiOn = false;
+    })
+  }
+})
